653bc2f870bd54dbfa50b2b454b8582ce8076dfd
Exigoll <exigoll@bk.ru>
2022-09-01 18:30:39 +0500

    feat: change city

--- js/helper.js
+++ js/helper.js
@@ -1,3 +1,6 @@
+import { createContent } from "./appContent.js";
+import { createHeader } from "./appHeader.js";
+
 export const directionOfwWind = (degree) => {
   if (degree>337.5) { return 'северный' };
   if (degree>292.5) { return 'северо-западный' };
@@ -12,4 +15,12 @@ export const directionOfwWind = (degree) => {
 
 export const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export const resetWeatherContent = (city, weather) => {
+  localStorage.setItem("city", JSON.stringify(city));
+  document.body.innerHTML = "";
+  const header = createHeader(city);
+  const content = createContent(weather);
+  document.body.append(header, content);
 }
