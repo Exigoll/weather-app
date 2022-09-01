cd975b27061e1a8bdd00a4f4b7f31d7e9ffd6ca8
Exigoll <exigoll@bk.ru>
2022-09-01 17:28:34 +0500

    feat: content

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 export const getWeatherData = async (city) => {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe76d59fdbf7579d5aadae264a9aa0cb`);
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe76d59fdbf7579d5aadae264a9aa0cb&lang=ru&units=metric`);
 
     return await response.json();
   } catch (error) {
